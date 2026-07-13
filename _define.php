01772377f592802a8a27894ecc8c972d1e4e85bc
Jean-Christian Denis <contact@jcdenis.fr>
2026-07-14 00:56:12 +0200

    Code review (phpstan max)

--- _define.php
+++ _define.php
@@ -14,6 +14,10 @@
  */
 declare(strict_types=1);
 
+if (!is_object($this) || !method_exists($this, 'registerModule') || !isset($this->id) || !is_string($this->id)) {
+    return;
+}
+
 $this->registerModule(
     'Arlequin',
     'Allows visitors choose a theme',
